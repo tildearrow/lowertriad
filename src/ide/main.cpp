1a55be0425ea5858585c133c634c55f3d9aa4398
tildearrow <tildearrow@users.noreply.github.com>
2016-03-28 21:53:06 -0500

    no more unistd, and also, disabling SDL init check

--- src/ide/main.cpp
+++ src/ide/main.cpp
@@ -1,26 +1,22 @@
 #include "includes.h"
-#include <unistd.h>
 SDL_Window* mainWindow;
 SDL_Renderer* mainRenderer;
 SDL_Event* event;
 
 int main() {
   // init everything
-  if (!SDL_Init(SDL_INIT_VIDEO)) {
-    printf("i'm sorry, but SDL can't be loaded: %s\n",SDL_GetError());
-    return 1;
-  }
+  SDL_Init(SDL_INIT_VIDEO);
   string title;
   title="LowerTriad";
-  mainWindow=SDL_CreateWindow(title.c_str(),0,0,1024,768,0);
+  mainWindow=SDL_CreateWindow(title.c_str(),0,0,1024,600,0);
   if (!mainWindow) {
     printf("i'm sorry, but window can't be created: %s\n",SDL_GetError());
     return 1;
   }
   mainRenderer=SDL_CreateRenderer(mainWindow,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
   while (1) {
+    SDL_RenderClear(mainRenderer);
     SDL_RenderPresent(mainRenderer);
   }
-  sleep(1);
   return 0;
 }
