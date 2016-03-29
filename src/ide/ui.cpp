a582ed9a47bf455d38df7f2223a34ae7eb130a03
tildearrow <tildearrow@users.noreply.github.com>
2016-03-28 21:53:58 -0500

    adding prototype UI code

---
+++ src/ide/ui.cpp
@@ -0,0 +1,13 @@
+#include "ui.h"
+
+void button::draw() {
+  SDL_RenderDrawRect(renderer,&coords);
+}
+
+void textfield::draw() {
+  
+}
+
+void label::draw() {
+  
+}
--- src/ide/ui.h
+++ src/ide/ui.h
@@ -1,11 +1,38 @@
-class button {
+#include "includes.h"
 
+class uielement {
+  public:
+    SDL_Rect coords;
+    SDL_Renderer* renderer;
 };
 
-class label {
-
+class button:uielement {
+  public:
+    string text, hint;
+    SDL_Color color, bordercolor;
+  void draw();
 };
 
-class textfield {
+class label:uielement {
+  public:
+    string text;
+    SDL_Color color;
+  void draw();
+};
 
+class textfield:uielement {
+  public:
+    string text, hint, deftext;
+    SDL_Color color, bordercolor, bgcolor;
+  void draw();
+};
+
+class uisystem {
+  std::vector<button> buttons;
+  std::vector<label> labels;
+  std::vector<textfield> textfields;
+  SDL_Renderer* renderer;
+public:
+  void addbutton(int x, int y, int w, int h, string text, string hint, SDL_Color color, SDL_Color bordercolor);
+  void drawall();
 };
