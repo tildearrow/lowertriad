970d78cc772b9d0c48e9461a4e0333a21eb2965f
tildearrow <tildearrow@users.noreply.github.com>
2016-04-27 18:12:59 -0500

    adding alpha channel to color selector

--- src/ide/gfxeditor.cpp
+++ src/ide/gfxeditor.cpp
@@ -5,45 +5,54 @@ void gfxeditor::mouse() {
     temppoint.x=*mX;
     temppoint.y=*mY;
     temprect.x=offX+24;
-    temprect.y=h-55;
+    temprect.y=h-75;
     temprect.w=256;
     temprect.h=10;
     if (SDL_PointInRect(&temppoint,&temprect)) {
       fg.r=*mX-offX-24;
     }
-    temprect.y=h-35;
+    temprect.y=h-55;
     if (SDL_PointInRect(&temppoint,&temprect)) {
       fg.g=*mX-offX-24;
     }
-    temprect.y=h-15;
+    temprect.y=h-35;
     if (SDL_PointInRect(&temppoint,&temprect)) {
       fg.b=*mX-offX-24;
     }
+    temprect.y=h-15;
+    if (SDL_PointInRect(&temppoint,&temprect)) {
+      fg.a=*mX-offX-24;
+    }
+
   }
 }
 
 void gfxeditor::draw() {
   SDL_SetRenderDrawColor(r,color[0].r,color[0].g,color[0].b,color[0].a);
-  SDL_RenderDrawLine(r,offX,h-64,w,h-64);
-  gf->draw(offX+8,h-60,color[0],0,0,0,"R");
-  gf->draw(offX+8,h-40,color[0],0,0,0,"G");
-  gf->draw(offX+8,h-20,color[0],0,0,0,"B");
+  SDL_RenderDrawLine(r,offX,h-84,w,h-84);
+  gf->draw(offX+8,h-80,color[0],0,0,0,"R");
+  gf->draw(offX+8,h-60,color[0],0,0,0,"G");
+  gf->draw(offX+8,h-40,color[0],0,0,0,"B");
+  gf->draw(offX+8,h-20,color[0],0,0,0,"A");
   // color sliders
   SDL_SetRenderDrawColor(r,color[0].r,color[0].g,color[0].b,color[0].a);
   temprect.x=offX+24;
-  temprect.y=h-55;
+  temprect.y=h-75;
   temprect.w=256;
   temprect.h=10;
+  SDL_RenderDrawRect(r,&temprect);
+  temprect.y=h-55;
   SDL_RenderDrawRect(r,&temprect);
   temprect.y=h-35;
   SDL_RenderDrawRect(r,&temprect);
   temprect.y=h-15;
   SDL_RenderDrawRect(r,&temprect);
   
-  SDL_RenderDrawPoint(r,offX+24+fg.r,h-56);
-  SDL_RenderDrawPoint(r,offX+24+fg.g,h-36);
-  SDL_RenderDrawPoint(r,offX+24+fg.b,h-16);
-  
+  SDL_RenderDrawPoint(r,offX+24+fg.r,h-76);
+  SDL_RenderDrawPoint(r,offX+24+fg.g,h-56);
+  SDL_RenderDrawPoint(r,offX+24+fg.b,h-36);
+  SDL_RenderDrawPoint(r,offX+24+fg.a,h-16);
+
   SDL_SetRenderDrawColor(r,fg.r,fg.g,fg.b,255);
   temprect.x=offX+288;
   temprect.y=h-58;
