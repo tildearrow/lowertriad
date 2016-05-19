20a2c2d6d096754125259cd76810743dba44d46e
tildearrow <tildearrow@users.noreply.github.com>
2016-05-18 20:37:10 -0500

    event editor buttons layout

--- src/ide/eteditor.cpp
+++ src/ide/eteditor.cpp
@@ -17,7 +17,15 @@ void eteditor::draw() {
   if (entitytype!=NULL) {
     f->draw(w-128, offY+2, color[0], 1, 0, false, "Events");
     SDL_RenderDrawLine(r, w, offY+20, w-256, offY+20);
+    SDL_RenderDrawLine(r, w, offY+40, w-256, offY+40);
     SDL_RenderDrawLine(r, w-256, offY, w-256, h);
+    
+    SDL_RenderDrawLine(r, w-85, offY+20, w-85, offY+40);
+    SDL_RenderDrawLine(r, w-171, offY+20, w-171, offY+40);
+    
+    f->draw(w-214, offY+22, color[0], 1, 0, 0, "Add");
+    f->draw(w-128, offY+22, color[0], 1, 0, 0, "Change");
+    f->draw(w-43, offY+22, color[0], 1, 0, 0, "Remove");
   }
 }
 
